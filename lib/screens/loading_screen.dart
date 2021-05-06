d41a8915cea1d9dc64719264843f02b157d4e656
Omar Emad <2001omaremad@gmail.com>
2021-05-06 04:22:06 +0200

    Getting data from open weather API

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -2,6 +2,7 @@ import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'location_screen.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -9,6 +10,14 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  void getWeatherData() async {
+    http.Response response = await http.get(
+      Uri.parse(
+          'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22'),
+    );
+    print(response.body);
+  }
+
   void getLocation() async {
     Location location = Location();
     await location.getCurrentLocation();
@@ -24,6 +33,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   @override
   Widget build(BuildContext context) {
+    getWeatherData();
     return Scaffold();
   }
 }
